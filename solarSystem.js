fd2535df4f04668b7e32530ccd1b0f39f31e25dc
Jose <josefbautista94@gmail.com>
2025-03-20 15:50:18 -0400

    Pushing the first half of the lab assignment

--- solarSystem.js
+++ solarSystem.js
@@ -12,27 +12,40 @@ const solarSystem = [
     { name: "Neptune", ringSystem: true, moons: ["Triton", "Nereid"] }
   ];
   // 1. Print the array of Jupiter's moons to the console (no for loop, just print the entire array).
-  
+  console.log(solarSystem[4].moons)
   
   // 2. Print the name of Neptune's moon "Nereid" to the console.
-  
+  console.log(solarSystem[7].moons[1])
   
   // 3. Add a new moon called "Endor" to Venus' moons array.
-  
-  
+  solarSystem[1].moons.push("Endor")
+  console.log(solarSystem[1].moons)
+
   // 4. Add a Pluto object to the solarSystem array using .push. The object should contain Pluto's name, ringSystem boolean, and moons array (which includes "Charon").
-  
-  
+  solarSystem.push({
+    name: "Pluto",
+    ringSystem : false,
+    moons :["Charon"]
+  })
+  console.log(solarSystem)
   // 5. Add a new key-value pair to the the Earth object: the key should be 'diameter', and the value should be Earth's diameter in miles represented as a string.
-  
+  solarSystem[2].diameter = "7926.2"
+  console.log(solarSystem[2])
   
   // 6. Change Mercury's ringSystem boolean to true.
-  
+  solarSystem[0].ringSystem = true;
+console.log(solarSystem[0])
   
   // 7. Change Uranus' moon "Umbriel" to "Oberon"
-  
-  
-  // 8. Iterate through the solarSystem array and print only the objects that have a ringSystem (where ringSystem: true), and ignore the others.
+solarSystem[6].moons.splice(3,1, "Oberon")
+console.log(solarSystem[6])
+
+// 8. Iterate through the solarSystem array and print only the objects that have a ringSystem (where ringSystem: true), and ignore the others.
+for( let key in solarSystem){
+    if(solarSystem[key].ringSystem === true){
+    console.log(solarSystem[key])
+}
+}
   //////////////////////////////
   // The Solar System, Part 2 //
   //////////////////////////////
